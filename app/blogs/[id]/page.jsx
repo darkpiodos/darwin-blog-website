e76533772175ad1165ab85402656f8564f0e7448
darwinpiodos <darwindivinopiodos21@gmail.com>
2025-02-11 00:08:03 +0800

    Successful creating of API to GET and POST request

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { assets, blog_data } from "@/Assets/assets";
+import Footer from "@/Components/Footer";
 import Image from "next/image";
+import Link from "next/link";
 import React, { use, useEffect, useState } from "react";
 
 const page = ({ params }) => {
@@ -25,12 +27,14 @@ const page = ({ params }) => {
     <>
       <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
         <div className="flex justify-between items-center">
-          <Image
-            src={assets.logo}
-            width={180}
-            alt="this is a logo"
-            className="w-[130px] sm:w-auto"
-          />
+          <Link href="/">
+            <Image
+              src={assets.logo}
+              width={180}
+              alt="this is a logo"
+              className="w-[130px] sm:w-auto"
+            />
+          </Link>
           <button className="flex items-center gap-2 front-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
             Get started <Image src={assets.arrow} alt="this is an arrow" />
           </button>
@@ -44,7 +48,7 @@ const page = ({ params }) => {
           <Image
             className="mx-auto mt-6 border border-white rounded-full"
             src={data.author_img}
-            alt=""
+            alt="this is an author image"
             width={60}
             height={60}
           />
@@ -56,7 +60,7 @@ const page = ({ params }) => {
       <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
         <Image
           src={data.image}
-          alt=""
+          alt="this is the blog image"
           width={1280}
           height={720}
           className="border-4 border-white "
@@ -118,12 +122,18 @@ const page = ({ params }) => {
             Share this article on social media
           </p>
           <div className="flex">
-            <Image src={assets.facebook_icon} width={50} alt="" />
-            <Image src={assets.twitter_icon} width={50} alt="" />
-            <Image src={assets.googleplus_icon} width={50} alt="" />
+            <Image src={assets.facebook_icon} width={50} alt="facebook icon" />
+            <Image src={assets.twitter_icon} width={50} alt="twitter icon" />
+            <Image
+              src={assets.googleplus_icon}
+              width={50}
+              alt="google plus icon"
+            />
           </div>
         </div>
       </div>
+
+      <Footer />
     </>
   ) : (
     <></>
